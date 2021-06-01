92234988d00202053687009d76bf5001c68a0e04
Matheus Rocha <matheusrochacamarg@gmail.com>
2021-05-31 22:26:39 -0300

    04 | Contexto e Audio

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,9 +1,10 @@
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
+import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router'
+
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
@@ -26,8 +27,6 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-    const router = useRouter();
-
     return (
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
@@ -62,8 +61,28 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit: 2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    });
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    });
+
     return {
-        paths: [],
+        paths: [{
+            params: {
+                slug: 'a-importancia-da-contribuicao-em-open-source'
+            }
+        }],
         fallback: 'blocking'
     }
 }
